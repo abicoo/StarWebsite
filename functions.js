d96a35ca91e52fba051a76619eb69e56a8e6abb1
abicoo <42435406+abicoo@users.noreply.github.com>
2023-09-22 19:45:59 +0530

    Add files via upload
    First POC

---
+++ functions.js
@@ -0,0 +1,8 @@
+function gotob(){
+    let botResultVar   =   document.getElementById("result_menu_bottom")
+    botResultVar.scrollIntoView()
+}
+function gotot(){
+    let topResultVar    =   document.getElementById("result_menu_top")
+    topResultVar.scrollIntoView()
+}
